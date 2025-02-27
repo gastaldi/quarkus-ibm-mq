74b9ce626c7fd907a2b834c2ed52a798e85d8872
George Gastaldi <gegastaldi@gmail.com>
2025-02-27 20:56:20 -0300

    Reading properties

--- deployment/src/main/java/io/quarkiverse/ibm/mq/deployment/IbmMqProcessor.java
+++ deployment/src/main/java/io/quarkiverse/ibm/mq/deployment/IbmMqProcessor.java
@@ -1,7 +1,20 @@
 package io.quarkiverse.ibm.mq.deployment;
 
+import com.ibm.mq.MQJavaComponent;
+import com.ibm.mq.jakarta.jms.MQJMSComponent;
+import com.ibm.mq.jmqi.remote.api.RemoteFAP;
+import com.ibm.msg.client.commonservices.componentmanager.ComponentManager;
+import com.ibm.msg.client.commonservices.j2se.J2SEComponent;
+import com.ibm.msg.client.jakarta.jms.internal.JMSComponent;
+import com.ibm.msg.client.jakarta.jms.internal.JmsFactoryFactoryImpl;
+import com.ibm.msg.client.jakarta.wmq.factories.WMQComponent;
+import com.ibm.msg.client.jakarta.wmq.factories.WMQFactoryFactory;
+import com.ibm.msg.client.jakarta.wmq.factories.admin.WMQJmsFactory;
+
+import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.builditem.FeatureBuildItem;
+import io.quarkus.deployment.builditem.nativeimage.ReflectiveClassBuildItem;
 
 class IbmMqProcessor {
 
@@ -11,4 +24,20 @@ class IbmMqProcessor {
     FeatureBuildItem feature() {
         return new FeatureBuildItem(FEATURE);
     }
+
+    @BuildStep
+    void registerForReflection(BuildProducer<ReflectiveClassBuildItem> producer) {
+        producer.produce(ReflectiveClassBuildItem.builder(
+                JmsFactoryFactoryImpl.class,
+                RemoteFAP.class,
+                WMQFactoryFactory.class,
+                WMQJmsFactory.class,
+                ComponentManager.class,
+                J2SEComponent.class,
+                MQJavaComponent.class,
+                MQJMSComponent.class,
+                JMSComponent.class,
+                WMQComponent.class).constructors().methods().fields().build());
+    }
+
 }
---
+++ runtime/src/main/java/io/quarkiverse/ibm/mq/runtime/MQResourceAdapterFactory.java
@@ -0,0 +1,117 @@
+package io.quarkiverse.ibm.mq.runtime;
+
+import java.util.Map;
+
+import javax.transaction.xa.XAResource;
+
+import jakarta.jms.ConnectionFactory;
+import jakarta.jms.Message;
+import jakarta.jms.MessageListener;
+import jakarta.jms.XAConnectionFactory;
+import jakarta.resource.ResourceException;
+import jakarta.resource.spi.ActivationSpec;
+import jakarta.resource.spi.BootstrapContext;
+import jakarta.resource.spi.ManagedConnectionFactory;
+import jakarta.resource.spi.ResourceAdapter;
+import jakarta.resource.spi.ResourceAdapterInternalException;
+import jakarta.resource.spi.endpoint.MessageEndpoint;
+import jakarta.resource.spi.endpoint.MessageEndpointFactory;
+
+import com.ibm.mq.jakarta.connector.ResourceAdapterImpl;
+import com.ibm.mq.jakarta.connector.inbound.ActivationSpecImpl;
+import com.ibm.mq.jakarta.connector.outbound.ManagedConnectionFactoryImpl;
+
+import io.quarkiverse.ironjacamar.ResourceAdapterFactory;
+import io.quarkiverse.ironjacamar.ResourceAdapterKind;
+import io.quarkiverse.ironjacamar.ResourceAdapterTypes;
+import io.quarkiverse.ironjacamar.runtime.endpoint.MessageEndpointWrapper;
+
+@ResourceAdapterKind("ibm-mq")
+@ResourceAdapterTypes(connectionFactoryTypes = { ConnectionFactory.class, XAConnectionFactory.class })
+public class MQResourceAdapterFactory implements ResourceAdapterFactory {
+
+    @Override
+    public ResourceAdapter createResourceAdapter(String id, Map<String, String> config) throws ResourceException {
+        ResourceAdapterImpl adapter = new ResourceAdapterImpl();
+        //TODO: Set the properties
+        return new ResourceAdapterWrapper(adapter, config);
+    }
+
+    @Override
+    public ManagedConnectionFactory createManagedConnectionFactory(String id, ResourceAdapter adapter)
+            throws ResourceException {
+        ResourceAdapterWrapper wrapper = (ResourceAdapterWrapper) adapter;
+        ManagedConnectionFactoryImpl factory = new ManagedConnectionFactoryImpl();
+        factory.setHostName(wrapper.config.get("host"));
+        factory.setPort(wrapper.config.get("port"));
+        factory.setQueueManager(wrapper.config.get("queue-manager"));
+        factory.setChannel(wrapper.config.get("channel"));
+        factory.setResourceAdapter(adapter);
+        return factory;
+    }
+
+    @Override
+    public ActivationSpec createActivationSpec(String id, ResourceAdapter adapter, Class<?> type, Map<String, String> config)
+            throws ResourceException {
+        ActivationSpecImpl activationSpec = new ActivationSpecImpl();
+        activationSpec.setResourceAdapter(adapter);
+        activationSpec.setUseJNDI(false);
+        return activationSpec;
+    }
+
+    @Override
+    public MessageEndpoint wrap(MessageEndpoint endpoint, Object resourceEndpoint) {
+        return new JMSMessageEndpoint(endpoint, (MessageListener) resourceEndpoint);
+    }
+
+    private static class ResourceAdapterWrapper implements ResourceAdapter {
+
+        private final ResourceAdapter delegate;
+        private final Map<String, String> config;
+
+        private ResourceAdapterWrapper(ResourceAdapter delegate, Map<String, String> config) {
+            this.delegate = delegate;
+            this.config = config;
+        }
+
+        @Override
+        public void start(BootstrapContext ctx) throws ResourceAdapterInternalException {
+            delegate.start(ctx);
+        }
+
+        @Override
+        public void stop() {
+            delegate.stop();
+        }
+
+        @Override
+        public void endpointActivation(MessageEndpointFactory endpointFactory, ActivationSpec spec) throws ResourceException {
+            delegate.endpointActivation(endpointFactory, spec);
+        }
+
+        @Override
+        public void endpointDeactivation(MessageEndpointFactory endpointFactory, ActivationSpec spec) {
+            delegate.endpointDeactivation(endpointFactory, spec);
+        }
+
+        @Override
+        public XAResource[] getXAResources(ActivationSpec[] specs) throws ResourceException {
+            return delegate.getXAResources(specs);
+        }
+    }
+
+    private static class JMSMessageEndpoint extends MessageEndpointWrapper implements MessageListener {
+
+        private final MessageListener listener;
+
+        public JMSMessageEndpoint(MessageEndpoint messageEndpoint, MessageListener listener) {
+            super(messageEndpoint);
+            this.listener = listener;
+        }
+
+        @Override
+        public void onMessage(Message message) {
+            listener.onMessage(message);
+        }
+    }
+}
