2476df4a5a1ce85e64f97d3ac4be435d187e6d33
George Gastaldi <gegastaldi@gmail.com>
2025-08-07 14:32:27 -0300

    Name is required

--- deployment/src/main/java/io/quarkiverse/ibm/mq/deployment/IbmMqProcessor.java
+++ deployment/src/main/java/io/quarkiverse/ibm/mq/deployment/IbmMqProcessor.java
@@ -76,6 +76,7 @@ class IbmMqProcessor {
 
         return DevServicesResultBuildItem.discovered()
                 .containerId(container.getContainerId())
+                .name(container.getContainerName())
                 .description("IBM MQ Container")
                 .config(configOverrides)
                 .build();
